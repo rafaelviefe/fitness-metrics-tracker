3428ad3ad499209d1fb0650ea652e8a083fffd47
Rafael Vieira Ferreira <161779899+rafaelviefe@users.noreply.github.com>
2026-08-23 19:47:44 -0300

    feat: Update `src/components/ui/toggle-group.tsx` to harmonize the `toggleItemUnselectedVariantClasses.secondary` styles with the `Button` component's `variantClasses.secondary` styles. (#282)

--- src/components/ui/toggle-group.tsx
+++ src/components/ui/toggle-group.tsx
@@ -79,7 +79,7 @@ const toggleItemUnselectedVariantClasses: Record<ToggleGroupItemVariant, string>
   outline:
     'border border-gray-200 bg-white hover:bg-gray-100 hover:text-gray-900 dark:border-gray-700 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50',
   secondary:
-    'bg-gray-100 text-gray-900 hover:bg-gray-200 dark:bg-gray-800 dark:text-gray-50 dark:hover:bg-gray-700',
+    'bg-neutral-100 text-neutral-900 hover:bg-neutral-100/80 dark:bg-neutral-800 dark:text-neutral-50 dark:hover:bg-neutral-800/80',
   ghost:
     'hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-gray-800 dark:hover:text-gray-50',
   link: 'text-gray-900 underline-offset-4 hover:underline dark:text-gray-50',
